4bf65e7a4830e9adfd30f9a690896fe311948911
Emmanuel0705 <emmanuelloluwatobi@gmail.com>
2020-04-08 06:50:16 +0100

    code refactory

--- src/estimator.js
+++ src/estimator.js
@@ -1,30 +1,62 @@
-const chalenges = ({ data, impact, severeImpact }) => {
-  // challenge 1
+// challenge 1
+const currentlyInfected = ({ data, impact, severeImpact }) => {
   impact.currentlyInfected = (data.reportedCases * 1) * 10;
   severeImpact.currentlyInfected = (data.reportedCases * 1) * 50;
+  return { impact, severeImpact };
+};
+const infectionsByRequestedTime = ({ data, impact, severeImpact }) => {
   impact.infectionsByRequestedTime = (impact.currentlyInfected * 1)
   * (2 ** (data.timeToElapse / 3));
   severeImpact.infectionsByRequestedTime = (severeImpact.currentlyInfected * 1)
   * (2 ** (data.timeToElapse / 3));
-  // challenge 2
+  return { impact, severeImpact };
+};
+// challenge 2
+const severeCasesByRequestedTime = ({ impact, severeImpact }) => {
   impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
-  impact.hospitalBedsByRequestedTime = impact.severeCasesByRequestedTime
-  - data.totalHospitalBeds;
   severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.15;
+  return { impact, severeImpact };
+};
+const hospitalBedsByRequestedTime = ({ data, impact, severeImpact }) => {
+  impact.hospitalBedsByRequestedTime = impact.severeCasesByRequestedTime
+  - data.totalHospitalBeds;    
   severeImpact.hospitalBedsByRequestedTime = severeImpact.severeCasesByRequestedTime
   - data.totalHospitalBeds;
-  // challenge 3
+  return { impact, severeImpact };
+};
+// challenge 3
+const casesForICUByRequestedTime = ({ impact, severeImpact }) => {
   impact.casesForICUByRequestedTime = impact.infectionsByRequestedTime * 0.05;
-  impact.casesForVentilatorsByRequestedTime = impact.infectionsByRequestedTime * 0.02;
   severeImpact.casesForICUByRequestedTime = severeImpact.infectionsByRequestedTime * 0.05;
+  return { impact, severeImpact };
+};
+const casesForVentilatorsByRequestedTime = ({ impact, severeImpact }) => {
+  impact.casesForVentilatorsByRequestedTime = impact.infectionsByRequestedTime * 0.02;  
   severeImpact.casesForVentilatorsByRequestedTime = severeImpact.infectionsByRequestedTime * 0.05;
+  return { impact, severeImpact };
+};
+const dollarsInFlight = ({ data, impact, severeImpact }) => {
   impact.dollarsInFlight = (impact.infectionsByRequestedTime * 1)
   * (data.region.avgDailyIncomeInPopulation * 1) * (data.region.avgDailyIncomeInUSD * 1)
   * (data.timeToElapse * 1);
   severeImpact.dollarsInFlight = severeImpact.infectionsByRequestedTime
   * data.region.avgDailyIncomeInPopulation * data.region.avgDailyIncomeInUSD
   * data.timeToElapse;
+  return { impact, severeImpact }
+};
+const chalenges = ({ data, impact, severeImpact }) => {
+  // challenge 1
+  currentlyInfected({ data, impact, severeImpact })
+  infectionsByRequestedTime({ data, impact, severeImpact })
+  // challenge 2
+  severeCasesByRequestedTime({ impact, severeImpact })
+  hospitalBedsByRequestedTime({ data, impact, severeImpact })
+  // challenge 3
+  casesForICUByRequestedTime({ impact, severeImpact })
+  casesForVentilatorsByRequestedTime({ impact, severeImpact })
+  dollarsInFlight({ data, impact, severeImpact })
   return { data, impact, severeImpact };
 };
+
 const covid19ImpactEstimator = (data) => chalenges({ data, impact: {}, severeImpact: {} });
 export default covid19ImpactEstimator;
