c547afef437750b61c6744b6b467b56696aa40a7
Emmanuel0705 <emmanuelloluwatobi@gmail.com>
2020-04-09 13:25:33 +0100

    Fixing Challenge 3 bugs

--- src/challenge-3.js
+++ src/challenge-3.js
@@ -1,13 +1,13 @@
 exports.casesForICUByRequestedTime = ({ impact, severeImpact }) => {
-  impact.casesForICUByRequestedTime = Math.trunc(impact.severeCasesByRequestedTime * 0.05);
+  impact.casesForICUByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.05);
   severeImpact.casesForICUByRequestedTime = Math.trunc(severeImpact
-    .severeCasesByRequestedTime * 0.05);
+    .infectionsByRequestedTime * 0.05);
   return { impact, severeImpact };
 };
 exports.casesForVentilatorsByRequestedTime = ({ impact, severeImpact }) => {
-  impact.casesForVentilatorsByRequestedTime = Math.trunc(impact.severeCasesByRequestedTime * 0.02);
+  impact.casesForVentilatorsByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.02);
   severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(severeImpact
-    .severeCasesByRequestedTime * 0.02);
+    .infectionsByRequestedTime * 0.02);
   return { impact, severeImpact };
 };
 exports.dollarsInFlight = ({ data, impact, severeImpact }) => {
