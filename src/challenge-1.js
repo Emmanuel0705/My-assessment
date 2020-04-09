d94e36ab324a02f0cbb5964e3a1fc1ad894a4432
Emmanuel0705 <emmanuelloluwatobi@gmail.com>
2020-04-09 04:50:44 +0100

    submitting challenge 1

--- src/challenge-1.js
+++ src/challenge-1.js
@@ -5,9 +5,9 @@ exports.currentlyInfected = ({ data, impact, severeImpact }) => {
 };
 exports.infectionsByRequestedTime = ({ data, impact, severeImpact }) => {
   data.timeToElapse *= 1;
-  if (data.periodType === 'months') data.timeToElapse *= 30;
-  if (data.periodType === 'weeks') data.timeToElapse *= 7;
   if (data.periodType === 'days') data.timeToElapse *= 1;
+  if (data.periodType === 'weeks') data.timeToElapse *= 7;
+  if (data.periodType === 'months') data.timeToElapse *= 30;
   impact.infectionsByRequestedTime = (impact.currentlyInfected * 1)
   * (2 ** (data.timeToElapse / 3));
   severeImpact.infectionsByRequestedTime = (severeImpact.currentlyInfected * 1)
