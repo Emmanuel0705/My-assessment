2a21896e202eeb0e8d7fb429fec7da8389b5237d
Emmanuel0705 <emmanuelloluwatobi@gmail.com>
2020-04-09 01:54:38 +0100

    submitting challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -14,9 +14,5 @@ const chalenges = ({ data, impact, severeImpact }) => {
   // dollarsInFlight({ data, impact, severeImpact });
   return { data, impact, severeImpact };
 };
-const data = {
-    region:{name: "Africa",avgAge: 19.7,avgDailyIncomeInUSD: 5,avgDailyIncomePopulation: 0.71},
-    periodType: "days",timeToElapse: 58,reportedCases: 674,population: 66622705, totalHospitalBeds: 1380614 
-}
 const covid19ImpactEstimator = (data) => chalenges({ data, impact: {}, severeImpact: {} });
 export default covid19ImpactEstimator;
