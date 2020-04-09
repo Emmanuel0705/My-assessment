d18617e7f5bd3cbd14b0ac43228c45da7a56b39b
Emmanuel0705 <emmanuelloluwatobi@gmail.com>
2020-04-09 11:55:04 +0100

    Fixing Challenge 3 bugs

--- src/challenge-3.js
+++ src/challenge-3.js
@@ -1,28 +1,28 @@
 exports.casesForICUByRequestedTime = ({ impact, severeImpact }) => {
-  // impact.casesForICUByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.05);
-  // severeImpact.casesForICUByRequestedTime = severeImpact
-  //   .infectionsByRequestedTime * 0.05;
+  impact.casesForICUByRequestedTime = impact.infectionsByRequestedTime * 0.05;
+  severeImpact.casesForICUByRequestedTime = severeImpact
+    .infectionsByRequestedTime * 0.05;
   return { impact, severeImpact };
 };
 exports.casesForVentilatorsByRequestedTime = ({ impact, severeImpact }) => {
-  // impact.casesForVentilatorsByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.02);
-  // severeImpact.casesForVentilatorsByRequestedTime = severeImpact
-  //   .infectionsByRequestedTime * 0.02;
+  impact.casesForVentilatorsByRequestedTime = impact.infectionsByRequestedTime * 0.02;
+  severeImpact.casesForVentilatorsByRequestedTime = severeImpact
+    .infectionsByRequestedTime * 0.02;
   return { impact, severeImpact };
 };
 exports.dollarsInFlight = ({ data, impact, severeImpact }) => {
-  // if (data.periodType === 'months') {
-  //   data.timeToElapse *= 30;
-  // } else if (data.periodType === 'weeks') {
-  //   data.timeToElapse *= 7;
-  // } else {
-  //   data.timeToElapse *= 1;
-  // }
-  // impact.dollarsInFlight = Math.trunc((impact.infectionsByRequestedTime * 1)
-  // * (data.region.avgDailyIncomePopulation * 1) * (data.region.avgDailyIncomeInUSD * 1)
-  // * (data.timeToElapse * 1));
-  // severeImpact.dollarsInFlight = Math.trunc(severeImpact.infectionsByRequestedTime
-  // * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD
-  // * data.timeToElapse);
+  if (data.periodType === 'months') {
+    data.timeToElapse *= 30;
+  } else if (data.periodType === 'weeks') {
+    data.timeToElapse *= 7;
+  } else {
+    data.timeToElapse *= 1;
+  }
+  impact.dollarsInFlight = Math.trunc((impact.infectionsByRequestedTime * 1)
+  * (data.region.avgDailyIncomePopulation * 1) * (data.region.avgDailyIncomeInUSD * 1)
+  * (data.timeToElapse * 1));
+  severeImpact.dollarsInFlight = Math.trunc(severeImpact.infectionsByRequestedTime
+  * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD
+  * data.timeToElapse);
   return { impact, severeImpact };
 };
