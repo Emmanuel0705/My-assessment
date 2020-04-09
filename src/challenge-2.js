dd16a32e17e1eef184e44e96713d1fb6920316f1
Emmanuel0705 <emmanuelloluwatobi@gmail.com>
2020-04-09 07:03:43 +0100

    fixing periodType Error

--- src/challenge-2.js
+++ src/challenge-2.js
@@ -4,9 +4,9 @@ exports.severeCasesByRequestedTime = ({ impact, severeImpact }) => {
   return { impact, severeImpact };
 };
 exports.hospitalBedsByRequestedTime = ({ data, impact, severeImpact }) => {
-  impact.hospitalBedsByRequestedTime = impact.severeCasesByRequestedTime
-  - (0.35 * data.totalHospitalBeds);
-  severeImpact.hospitalBedsByRequestedTime = severeImpact.severeCasesByRequestedTime
-  - (0.35 * data.totalHospitalBeds);
+  impact.hospitalBedsByRequestedTime = Math.trunc(0.35 * data.totalHospitalBeds)
+  - impact.severeCasesByRequestedTime;
+  severeImpact.hospitalBedsByRequestedTime = Math.trunc(0.35 * data.totalHospitalBeds)
+  - severeImpact.severeCasesByRequestedTime;
   return { impact, severeImpact };
 };
