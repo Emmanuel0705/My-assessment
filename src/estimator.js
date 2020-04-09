3a7546506b66c4d50869a96d3800900f3f875bc1
Emmanuel0705 <emmanuelloluwatobi@gmail.com>
2020-04-09 02:07:06 +0100

    submitting challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,7 +1,5 @@
 
-const chalengeOne = require('./challenge-1')
-const chalengeTwo = require('./challenge-2')
-const chalengeThree = require('./challenge-3')
+const chalengeOne = require('./challenge-1');
 
 const chalenges = ({ data, impact, severeImpact }) => {
   // challenge 1
